517d1e301ac24d7dad2c0b809890fa1896986c01
Robert Sainton <robertsainton@outlook.com>
2022-08-29 00:27:01 -0400

    bonusless assignment completed

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Article from "./Article"
 
-function ArticleList ({article}) {
-     const listElements = article.map((item) =>{
+function ArticleList ({posts}) {
+     const listElements = posts.map((item) =>{
         return <Article key={item.id} title={item.title} date={item.date} preview={item.preview}/>
      })
     return (
